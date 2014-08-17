0d7c85909f677249ad975265b66e753fe398fcd0
Jack Beckitt-Marshall <jack@pictura.co.uk>
2014-08-17 19:35:15 +0100

    Adds close-to-final solution to the alarm project.

--- Python/alarm/timer.py
+++ Python/alarm/timer.py
@@ -10,6 +10,9 @@ parser.add_argument("--hours", help="Set the time in hours", nargs="?", type=int
 
 args = parser.parse_args()
 
+pygame.init()
+pygame.mixer.init()
+sound = pygame.mixer.Sound("alarm.ogg")
 
 time = 0
 if args.seconds >= 0:
@@ -28,20 +31,41 @@ trueseconds = (time - (truehours * 3600) - (trueminutes * 60)) / 1
 
 print "Timer set for {0} hours, {1} minutes, and {2} seconds".format(truehours, trueminutes, trueseconds)
 
-pygame.mixer.init()
-pygame.display.set_mode((1, 1))
-pygame.mixer.music.load("alarm.ogg")
+
+
 def alarmsound():
+	### Alarm window code
 	
-	pygame.mixer.music.play()
-	while pygame.mixer.music.get_busy() == True:
-		continue
-		while True:
-			for event in pygame.event.get():
-				if event.type == KEYDOWN:
-					if event.key == K_SPACE:
-						pygame.mixer.music.stop()
+	# Initialises the alarm window
+	window = pygame.display.set_mode((250, 50))
+	pygame.display.set_caption("Alarm Window")
+	
+	# Fills the alarm window background
+	background = pygame.Surface(window.get_size())
+	background = background.convert()
+	background.fill((250, 250, 250))
+	
+	# Displays text in alarm window
+	font = pygame.font.Font(None, 24)
+	text = font.render("Press SPACE to snooze alarm.", 1, (10,10,10))
+	textpos = text.get_rect()
+	textpos.centerx = background.get_rect().centerx
+	background.blit(text, textpos)
+	
+	# Blits everything to the screen
+	window.blit(background, (0, 0))
+	pygame.display.flip()
+	
+	sound.play()
+	print "Press SPACE bar to snooze the alarm"
+	sound_on = True
+	while sound_on:
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					sound_on = False
+				else:
+					sound.play()
 			
 t = Timer(time, alarmsound)
 t.start()
-print t
